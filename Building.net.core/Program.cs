e58b2a5805e829f84cdda8d31a2bd3a06117a136
tdu819_Aleksey <testdrive819@gmail.com>
2021-02-20 19:47:22 +0200

    Building Creator added

--- Building.net.core/Program.cs
+++ Building.net.core/Program.cs
@@ -31,23 +31,21 @@ namespace District.CLI
             //}
 
 
-            //IPersonService PersonService = new PersonService();
-            //var res = PersonService.CreatePerson(new ApartmentModel()
+            IPersonService PersonService = new PersonService();
+            //var res = PersonService.CreatePerson(new PersonModel()
             //{
-            //    ApartmentNumber = 1,
-            //    BuildingId = 1,
-            //    IsOwn = false,
-            //    SquareSize = 12,
-            //    EntranceId = 1,
-            //    PersonId = 1,
-            //});
+            //    //Id = 0,
+            //    Name = "Name2",
+            //    PhoneNumber = 0670000002,
+            //}); 
             //res.Wait();
-            //Task<List<PersonModel>> persons = PersonService.GetAllPersons();
-            //persons.Wait();
-            //foreach (var person in persons.Result)
-            //{
-            //    Console.WriteLine($"{person.Name}, {person.SquareSize}, {person.IsOwn} ");
-            //}
+
+            Task<List<PersonModel>> persons = PersonService.GetAllPersons();
+            persons.Wait();
+            foreach (var person in persons.Result)
+            {
+                Console.WriteLine($"{person.Id}, {person.Name}, {person.PhoneNumber}");
+            }
 
 
 
